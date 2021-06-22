4fd8182c00abaa3e987fa672d43635c5f77fb5af
mac-Jolen <s2057809@ed.ac.uk>
2021-06-22 22:24:27 +0100

    6.22 leetcode 22题

---
+++ Offer 38..cpp
@@ -0,0 +1,36 @@
+//
+// Created by Jolen on 2021/6/21.
+//
+#include<vector>
+#include<iostream>
+#include<queue>
+#include<set>
+using namespace std;
+vector<string> ans;
+void dfs(string s,int x){
+    if(x==s.size()-1)
+        ans.push_back(s);
+    set<char> temp;
+    for(int i=x;i<s.size();i++)
+    {
+        if(temp.find(s[i])==temp.end())
+        {
+            temp.insert(s[i]);
+            swap(s[i],s[x]);
+            dfs(s,x+1);
+            swap(s[i],s[x]);
+        }
+    }
+}
+vector<string> permutation(string s) {
+    sort(s.begin(),s.end());
+    dfs(s,0);
+    return ans;
+}
+
+int main()
+{
+    auto ans = permutation("aab");
+    for(auto i:ans)
+        cout<<i<<endl;
+}
---
+++ leetcode.md
@@ -0,0 +1,34 @@
+#### [222. 完全二叉树的节点个数](https://leetcode-cn.com/problems/count-complete-tree-nodes/)
+
+递归+位运算
+
+二分查找
+
+
+
+#### [15. 三数之和](https://leetcode-cn.com/problems/3sum/)
+
+固定+双指针
+
+
+
+#### [1. 两数之和](https://leetcode-cn.com/problems/two-sum/)
+
+哈希表（map）
+
+
+
+#### [25. K 个一组翻转链表](https://leetcode-cn.com/problems/reverse-nodes-in-k-group/)
+
+迭代/递归 
+
+
+
+#### [11. 盛最多水的容器](https://leetcode-cn.com/problems/container-with-most-water/)
+
+双指针
+
+
+136. Single Number
+位运算
+     
