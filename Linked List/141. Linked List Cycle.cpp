8e9ee5c2fa08714772b7df16b867eedb252dfd9a
mac-Jolen <s2057809@ed.ac.uk>
2021-10-15 01:36:09 +0100

    10.14

---
+++ Linked List/141. Linked List Cycle.cpp
@@ -0,0 +1,43 @@
+//
+// Created by Jolen on 2021/10/14.
+//
+
+#include <iostream>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+
+//bool hasCycle(ListNode *head) {
+//    unordered_set<ListNode *> listSet;
+//    while (head) {
+//        if (listSet.count(head))
+//            return true;
+//        else
+//            listSet.insert(head);
+//            head = head->next
+//    }
+
+//    return false;
+//}
+bool hasCycle(ListNode *head) {
+    if(!head||!head->next)
+        return false;
+    ListNode *slow = head, *fast = head->next;
+    while (fast && fast->next) {
+        if (slow == fast)
+            return true;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return false;
+}
---
+++ Linked List/23. Merge k Sorted Lists.cpp
@@ -0,0 +1,94 @@
+//
+// Created by Jolen on 2021/10/15.
+//
+
+#include <vector>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// iteration
+//ListNode* mergeKLists(vector<ListNode*>& lists) {
+//    if(lists.empty())
+//        return nullptr;
+//
+//    ListNode* head = lists[0];
+//    ListNode* sum = new ListNode(-1);
+//    sum->next = head;
+//    ListNode* ans = sum;
+//    for(int i = 1;i<=lists.size();i++){
+//        ListNode* head = sum->next;
+//        ListNode* node = lists[i];
+//        while(head&&node){
+//            if(head->val<node->val)
+//            {
+//                sum->next = head;
+//                head = head->next;
+//            }else{
+//                sum->next = node;
+//                node =node->next;
+//            }
+//            sum  = sum->next;
+//        }
+//        sum->next = head?head:node;
+//        sum  = ans;
+//    }
+//    return ans->next;
+//}
+
+
+// recursive
+ListNode* mergeKLists(vector<ListNode*>& lists) {
+    if(lists.empty())
+        return nullptr;
+
+    ListNode* head = lists[0];
+    ListNode* sum = new ListNode(-1);
+    sum->next = head;
+    ListNode* ans = sum;
+    for(int i = 1;i<=lists.size();i++){
+        ListNode* head = sum->next;
+        ListNode* node = lists[i];
+        while(head&&node){
+            if(head->val<node->val)
+            {
+                sum->next = head;
+                head = head->next;
+            }else{
+                sum->next = node;
+                node =node->next;
+            }
+            sum  = sum->next;
+        }
+        sum->next = head?head:node;
+        sum  = ans;
+    }
+
+    return ans->next;
+}
+
+
+
+int main()
+{
+    ListNode *a, *b, *c, *d,*e;
+    a = new ListNode;
+    b = new ListNode;
+    c = new ListNode;
+    d = new ListNode;
+    e = new ListNode;
+    a->val = 1, b->val = 2, c->val = 3, d->val = 4, e->val = 5;
+    a->next = b, b->next = c, c->next = d,d->next=e,e->next= nullptr;
+    removeNthFromEnd(a,2);
+    while(a!= nullptr)
+    {
+        cout<<a->val;
+        a = a->next;
+    }
+}
