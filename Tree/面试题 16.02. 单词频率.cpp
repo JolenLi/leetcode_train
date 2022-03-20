2ce10dfc2e6d404c8711053ea429c3a7d1e74ac2
mac-Jolen <s2057809@ed.ac.uk>
2022-03-20 21:49:06 +0000

    3。20

---
+++ Tree/面试题 16.02. 单词频率.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Jolen on 2022/3/20.
+//
+//// 哈希表
+//class WordsFrequency {
+//public:
+//    unordered_map<string,int> freq;
+//    WordsFrequency(vector<string>& book) {
+//        for(string &word:book){
+//            freq[word]++;
+//        }
+//    }
+//
+//    int get(string word) {
+//        return freq[word];
+//    }
+//};
