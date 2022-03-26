80fd3d89efa938ccfc1bd34d41f708870084625b
mac-Jolen <s2057809@ed.ac.uk>
2022-03-26 01:51:07 +0000

    3。24

--- main.cpp
+++ main.cpp
@@ -22,30 +22,22 @@
 
 using namespace std;
 
-
-int getMin(int a, int b, int c) {
-    return min(min(a, b), c);
-}
-
-int minDistance(string word1, string word2) {
-    int m = word1.size(), n = word2.size();
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-    for (int i = 0; i <= m; i++)
-        dp[i][0] = i;
-    for (int j; j <= n; j++)
-        dp[0][j] = j;
-
-    for (int i = 1; i <= m; i++)
-        for (int j = 1; j <= n; j++) {
-            if (word1[i - 1] == word2[j - 1])
-                dp[i][j] = getMin(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1] - 1) + 1;
-            else
-                dp[i][j] = getMin(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
-        }
-    return dp[m][n];
+int method(vector<int> nums) {
+    int maxi = 0, sum = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        sum = max(sum + nums[i], 0);
+        maxi = max(maxi, sum);
+        cout << i << " " << maxi << endl;
+    }
+    cout << maxi << endl;
+    for (int i = 0; i < nums.size() - 1; i++) {
+        sum = max(sum + nums[i], nums[i]);
+        maxi = max(maxi, sum);
+    }
+    return maxi;
 }
 
 int main() {
-    string word1 = "horse", word2 = "ros";
-    cout << minDistance(word1, word2);
+    vector<int> nums = {5, -3, 5};
+    cout << method(nums) << endl;
 }
